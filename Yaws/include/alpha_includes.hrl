ab2e59b0c3c6fc61cc9e5263ace7e7d0b5d0eeec
Gokul <chandu.gokul138@gmail.com>
2012-11-12 18:14:01 +0100

    fix typo alpha_includes.erl

--- Yaws/include/alpha_includes.hrl
+++ Yaws/include/alpha_includes.hrl
@@ -1,1 +1,1 @@
--record(query,{country=Sweden,city=undefined,price=0,rooms=0,select=undefined,area=0}). %% price = 0-5 catagerios, rooms = 0-5 cat,area = 0-12
+-record(query,{country="Sweden",city=undefined,price=0,rooms=0,select=undefined,area=0}). %% price = 0-5 catagerios, rooms = 0-5 cat,area = 0-12
