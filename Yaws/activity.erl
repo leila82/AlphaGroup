6a978a0e3dd7d025087dd34d7de8e3398b859f85
Gokul <chandu.gokul138@gmail.com>
2012-11-18 21:59:04 +0100

    todo activity.erl updated

--- Yaws/activity.erl
+++ Yaws/activity.erl
@@ -1,5 +1,7 @@
 -module(activity).
 -author("Gokul").
+-define(DB_IP,"127.0.0.1").
+-define(DB_PN,"5984").
 -compile(export_all).
 
 %% This module is for yaws.conf file to include in runmod.
@@ -8,4 +10,5 @@
 
 
 start()->
+%%TODO - Check with the ping weather couch is been started on local server or not
     ok.
