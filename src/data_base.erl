c089b456b39fe114d5216cb0be894eb54357404b
Gokul <chandu.gokul138@gmail.com>
2012-12-09 20:12:29 +0100

    updated UI, Database, activity

--- src/data_base.erl
+++ src/data_base.erl
@@ -12,6 +12,19 @@
 -define(ViewClass,"class").
 %% View id need to be defined according to the quiry made so all the maps needed to be listed here.
 %% Test
+
+-define(NoObjects,200).
+
+-define(DD_Repo,"repo_dd"). %% repository for design document
+
+-define(Gothenburg,"gothenburg").
+-define(Malmo,"malmo").
+-define(Stockholm,"stockholm").
+
+-define(Gothenburgs,"gothenburg_s").
+-define(Malmos,"malmo_s").
+-define(Stockholms,"stockholm_s").
+
 -define(ViewId612,"612").
 -define(ViewId612i,"612i").
 -define(ViewId6121,"6121").
@@ -36,19 +49,44 @@
 -define(ViewId362,"362").
 -define(ViewId363,"363").
 
-%%-compile([create_database/1]).
+-define(Limit1LS,"undefined").
+-define(Limit1HS,"undefined").
+-define(Limit2LS,"undefined").
+-define(Limit2HS,"undefined").
+-define(Limit3LS,"undefined").
+-define(Limit3HS,"undefined").
+-define(Limit4LS,"undefined").
+-define(Limit4HS,"undefined").
+-define(Limit5LS,"undefined").
+-define(Limit5HS,"undefined").
+
+-define(Limit1LR,0).
+-define(Limit1HR,3000).
+-define(Limit2LR,3000).
+-define(Limit2HR,6000).
+-define(Limit3LR,6000).
+-define(Limit3HR,12000).
+-define(Limit4LR,12000).
+%%-define(Limit4HR,"undefined"). %% Not being used in the current version
+%%-define(Limit5LR,"undefined"). %% Not being used in the current version
+%%-define(Limit5HR,"undefined"). %% Not being used in the current version
+
+
+%%-export([create_database/1,delete_database/1,get_rdata/1,get_sdata/1]).
 -compile(export_all).
 
 
-%% @Key -> {City,Rent,NoRooms}
-get_data({City,Rent,NoRooms})->
-  %  {City,Rent,NoRooms} = Key,
-    case City of 
-	"Göteborg" ->
-	    get_gothenburg_rdata(Rent,NoRooms);
-	_ ->
-		"Ooo Krishna we didn't implement it yet"
-    end.
+create_doc(DB_Name,Doc)->
+case erlang_couchdb:create_document({"127.0.0.1", 5984}, DB_Name, Doc) of
+    ok ->
+	ok;
+    {json,{struct,[{<<"error">>,<<"not_found">>},{<<"reason">>,<<"no_db_file">>}]}} ->
+	throw("Application Error: an AH0bdat002 tag is thrown");
+    Rev ->
+	%throw("Application Error: an AH0bdat003 tag is thrown")
+	Rev
+end.
+
 
 %% @spec create_database(DBServer::server_address(), Database::string()) ->  ok | {error, Reason::any()}
 %%
@@ -58,7 +96,6 @@ get_data({City,Rent,NoRooms})->
 %% DB_Name = name of the data base you want to create
 create_database(DB_Name) when is_list(DB_Name)->
         erlang_couchdb:create_database({?DB_IP, ?DB_PN}, DB_Name).
-
 
 %% @spec create_database(DBServer::server_address(), Database::string()) ->  ok | {error, Reason::any()}
 %%
@@ -73,6 +110,11 @@ delete_database(DB_Name)->
     List_OO = lists:subtract(List_ad,List_dd),
     process_n_delete(DB_Name,List_OO).
 
+%% compaction is triggered, for elements and views
+compact_database(DB_Name)->
+    Uri = "/"++DB_Name++"/_compact",
+    erlang_couchdb:raw_request("POST",?DB_IP,?DB_PN,Uri,[]).
+
 get_alldocs(DB_Name) when is_list(DB_Name)->
     Uri = "/"++DB_Name++"/_all_docs",
     erlang_couchdb:raw_request("GET",?DB_IP,?DB_PN,Uri,[]).
@@ -82,58 +124,99 @@ get_designs(DB_Name) when is_list(DB_Name)->
     erlang_couchdb:raw_request("GET",?DB_IP,?DB_PN,Uri,[]).
 
 
-
-get_gothenburg_rdata(Rent,NoRooms) when Rent>6000,Rent<12000,NoRooms>3->
-    erlang_couchdb:invoke_view({?DB_IP,?DB_PN},"gothenburg",?ViewClass,?ViewId612i,[]);
-get_gothenburg_rdata(Rent,NoRooms) when Rent>6000,Rent<12000,NoRooms==1->
-    erlang_couchdb:invoke_view({?DB_IP,?DB_PN},"gothenburg",?ViewClass,?ViewId6121,[]);
-get_gothenburg_rdata(Rent,NoRooms) when Rent>6000,Rent<12000,NoRooms==2->
-    erlang_couchdb:invoke_view({?DB_IP,?DB_PN},"gothenburg",?ViewClass,?ViewId6122,[]);
-get_gothenburg_rdata(Rent,NoRooms) when Rent>6000,Rent<12000,NoRooms==3->
-    erlang_couchdb:invoke_view({?DB_IP,?DB_PN},"gothenburg",?ViewClass,?ViewId6123,[]);
-get_gothenburg_rdata(Rent,_NoRooms) when Rent>6000,Rent<12000->
-    erlang_couchdb:invoke_view({?DB_IP,?DB_PN},"gothenburg",?ViewClass,?ViewId612,[]);
-
-
-get_gothenburg_rdata(Rent,NoRooms) when Rent>0,Rent<3000,NoRooms>3->
-    erlang_couchdb:invoke_view({?DB_IP,?DB_PN},"gothenburg",?ViewClass,?ViewId03i,[]);
-get_gothenburg_rdata(Rent,NoRooms) when Rent>0,Rent<3000,NoRooms==1->
-    erlang_couchdb:invoke_view({?DB_IP,?DB_PN},"gothenburg",?ViewClass,?ViewId031,[]);
-get_gothenburg_rdata(Rent,NoRooms) when Rent>0,Rent<3000,NoRooms==2->
-    erlang_couchdb:invoke_view({?DB_IP,?DB_PN},"gothenburg",?ViewClass,?ViewId032,[]);
-get_gothenburg_rdata(Rent,NoRooms) when Rent>0,Rent<3000,NoRooms==3->
-    erlang_couchdb:invoke_view({?DB_IP,?DB_PN},"gothenburg",?ViewClass,?ViewId033,[]);
-get_gothenburg_rdata(Rent,_NoRooms) when Rent>0,Rent<3000->
-    erlang_couchdb:invoke_view({?DB_IP,?DB_PN},"gothenburg",?ViewClass,?ViewId03,[]);
+%% @Key -> {City,Rent,NoRooms}
+get_rdata({City,Rent,NoRooms})->
+  %  {City,Rent,NoRooms} = Key,
+    case City of 
+	"Göteborg" ->
+	    get_rdata(?Gothenburg,Rent,NoRooms);
+	"Malmö" ->
+	    get_rdata(?Malmo,Rent,NoRooms);
+	"Stockholm" ->
+	     get_rdata(?Stockholm,Rent,NoRooms);
+	_ ->
+	    []
+%%	    {error,"feature not available yet"}
+%%	    "Ooo Krishna we didn't implement it yet"
+    end.
 
 
-get_gothenburg_rdata(Rent,NoRooms) when Rent>12000,NoRooms>3->
-    erlang_couchdb:invoke_view({?DB_IP,?DB_PN},"gothenburg",?ViewClass,?ViewId12i,[]);
-get_gothenburg_rdata(Rent,NoRooms) when Rent>12000,NoRooms==1->
-    erlang_couchdb:invoke_view({?DB_IP,?DB_PN},"gothenburg",?ViewClass,?ViewId121,[]);
-get_gothenburg_rdata(Rent,NoRooms) when Rent>12000,NoRooms==2->
-    erlang_couchdb:invoke_view({?DB_IP,?DB_PN},"gothenburg",?ViewClass,?ViewId122,[]);
-get_gothenburg_rdata(Rent,NoRooms) when Rent>12000,NoRooms==3->
-    erlang_couchdb:invoke_view({?DB_IP,?DB_PN},"gothenburg",?ViewClass,?ViewId123,[]);
-get_gothenburg_rdata(Rent,_NoRooms) when Rent>12000->
-    erlang_couchdb:invoke_view({?DB_IP,?DB_PN},"gothenburg",?ViewClass,?ViewId12,[]);
+%% @Key -> {City,Rent,NoRooms}
+get_sdata({City,Rent,Price,NoRooms})->
+  %  {City,Rent,NoRooms} = Key,
+    case City of 
+	"Göteborg" ->
+	    get_sdata(?Gothenburg,Rent,Price,NoRooms);
+	"Malmö" ->
+	    get_sdata(?Malmo,Rent,Price,NoRooms);
+	"Stockholm" ->
+	     get_sdata(?Stockholm,Rent,Price,NoRooms);
+	_ ->
+	    {error,"feature not available yet"}
+%%	    "Ooo Krishna we didn't implement it yet"
+    end.
+
+%% UI update, Sell rent should be mentioned to only with max rent
+
+get_sdata(City,MaxRent,Price,NoRooms)->
+    %% invoke booli api here 
+    case catch json_handler:decode_string(booli:make_request(City,?NoObjects,MaxRent,Price,NoRooms)) of
+	{'EXIT',function_clause}->
+	   [];
+	{'EXIT',"invalid data input of JSON string in decode_string"} ->
+	    [];
+	[H|T] ->
+	    [H|T];
+	_ ->
+	    []
+    end.
+
+get_rdata(City,Rent,NoRooms) when Rent>6000,Rent<12000,NoRooms>3->
+    erlang_couchdb:invoke_view({?DB_IP,?DB_PN},City,?ViewClass,?ViewId612i,[]);
+get_rdata(City,Rent,NoRooms) when Rent>6000,Rent<12000,NoRooms==1->
+    erlang_couchdb:invoke_view({?DB_IP,?DB_PN},City,?ViewClass,?ViewId6121,[]);
+get_rdata(City,Rent,NoRooms) when Rent>6000,Rent<12000,NoRooms==2->
+    erlang_couchdb:invoke_view({?DB_IP,?DB_PN},City,?ViewClass,?ViewId6122,[]);
+get_rdata(City,Rent,NoRooms) when Rent>6000,Rent<12000,NoRooms==3->
+    erlang_couchdb:invoke_view({?DB_IP,?DB_PN},City,?ViewClass,?ViewId6123,[]);
+get_rdata(City,Rent,_NoRooms) when Rent>6000,Rent<12000->
+    erlang_couchdb:invoke_view({?DB_IP,?DB_PN},City,?ViewClass,?ViewId612,[]);
 
 
-get_gothenburg_rdata(Rent,NoRooms) when Rent>3000,Rent<6000,NoRooms>3->
-    erlang_couchdb:invoke_view({?DB_IP,?DB_PN},"gothenburg",?ViewClass,?ViewId36i,[]);
-get_gothenburg_rdata(Rent,NoRooms) when Rent>3000,Rent<6000,NoRooms==1->
-    erlang_couchdb:invoke_view({?DB_IP,?DB_PN},"gothenburg",?ViewClass,?ViewId361,[]);
-get_gothenburg_rdata(Rent,NoRooms) when Rent>3000,Rent<6000,NoRooms==2->
-    erlang_couchdb:invoke_view({?DB_IP,?DB_PN},"gothenburg",?ViewClass,?ViewId362,[]);
-get_gothenburg_rdata(Rent,NoRooms) when Rent>3000,Rent<6000,NoRooms==3->
-    erlang_couchdb:invoke_view({?DB_IP,?DB_PN},"gothenburg",?ViewClass,?ViewId363,[]);
-get_gothenburg_rdata(Rent,_NoRooms) when Rent>3000,Rent<6000->
-    erlang_couchdb:invoke_view({?DB_IP,?DB_PN},"gothenburg",?ViewClass,?ViewId36,[]).
+get_rdata(City,Rent,NoRooms) when Rent>0,Rent<3000,NoRooms>3->
+    erlang_couchdb:invoke_view({?DB_IP,?DB_PN},City,?ViewClass,?ViewId03i,[]);
+get_rdata(City,Rent,NoRooms) when Rent>0,Rent<3000,NoRooms==1->
+    erlang_couchdb:invoke_view({?DB_IP,?DB_PN},City,?ViewClass,?ViewId031,[]);
+get_rdata(City,Rent,NoRooms) when Rent>0,Rent<3000,NoRooms==2->
+    erlang_couchdb:invoke_view({?DB_IP,?DB_PN},City,?ViewClass,?ViewId032,[]);
+get_rdata(City,Rent,NoRooms) when Rent>0,Rent<3000,NoRooms==3->
+    erlang_couchdb:invoke_view({?DB_IP,?DB_PN},City,?ViewClass,?ViewId033,[]);
+get_rdata(City,Rent,_NoRooms) when Rent>0,Rent<3000->
+    erlang_couchdb:invoke_view({?DB_IP,?DB_PN},City,?ViewClass,?ViewId03,[]);
 
 
-get_gothenburg_sdata()->
-    ok.
+get_rdata(City,Rent,NoRooms) when Rent>12000,NoRooms>3->
+    erlang_couchdb:invoke_view({?DB_IP,?DB_PN},City,?ViewClass,?ViewId12i,[]);
+get_rdata(City,Rent,NoRooms) when Rent>12000,NoRooms==1->
+    erlang_couchdb:invoke_view({?DB_IP,?DB_PN},City,?ViewClass,?ViewId121,[]);
+get_rdata(City,Rent,NoRooms) when Rent>12000,NoRooms==2->
+    erlang_couchdb:invoke_view({?DB_IP,?DB_PN},City,?ViewClass,?ViewId122,[]);
+get_rdata(City,Rent,NoRooms) when Rent>12000,NoRooms==3->
+    erlang_couchdb:invoke_view({?DB_IP,?DB_PN},City,?ViewClass,?ViewId123,[]);
+get_rdata(City,Rent,_NoRooms) when Rent>12000->
+    erlang_couchdb:invoke_view({?DB_IP,?DB_PN},City,?ViewClass,?ViewId12,[]);
 
+
+get_rdata(City,Rent,NoRooms) when Rent>3000,Rent<6000,NoRooms>3->
+    erlang_couchdb:invoke_view({?DB_IP,?DB_PN},City,?ViewClass,?ViewId36i,[]);
+get_rdata(City,Rent,NoRooms) when Rent>3000,Rent<6000,NoRooms==1->
+    erlang_couchdb:invoke_view({?DB_IP,?DB_PN},City,?ViewClass,?ViewId361,[]);
+get_rdata(City,Rent,NoRooms) when Rent>3000,Rent<6000,NoRooms==2->
+    erlang_couchdb:invoke_view({?DB_IP,?DB_PN},City,?ViewClass,?ViewId362,[]);
+get_rdata(City,Rent,NoRooms) when Rent>3000,Rent<6000,NoRooms==3->
+    erlang_couchdb:invoke_view({?DB_IP,?DB_PN},City,?ViewClass,?ViewId363,[]);
+get_rdata(City,Rent,_NoRooms) when Rent>3000,Rent<6000->
+    erlang_couchdb:invoke_view({?DB_IP,?DB_PN},City,?ViewClass,?ViewId36,[]).
 
 
 %%@private
@@ -144,3 +227,7 @@ process_n_delete(DB_Name,[{struct,H}|T])->
     [{_,ID},{_,_Key},{_,{struct,[{_,Rev}]}}] = H,
     erlang_couchdb:delete_document({?DB_IP,?DB_PN},DB_Name,ID,Rev),
     process_n_delete(DB_Name,T).
+
+
+update_code()->
+    ok.
